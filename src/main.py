39f8f6d941e69c25a88782626044132a6758da3f
Luca Baffa <luca@openbookpublishers.com>
2021-04-13 11:12:42 +0100

    manage output file from within the class

--- src/main.py
+++ src/main.py
@@ -6,11 +6,10 @@ from pdf import Pdf
 def main():
     # DEV
     pdf_path = path.abspath('test.pdf')
-    out_pdf_path = path.abspath('out.pdf')
     rect = [748, 9, 1190, 672]
 
     pdf = Pdf(pdf_path)
-    pdf.set_cropbox(out_pdf_path, rect)
+    pdf.set_cropbox(rect)
 
 
 if __name__ == '__main__':
--- src/pdf.py
+++ src/pdf.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import fitz
+import tempfile
 
 
 class Pdf:
@@ -12,11 +13,12 @@ class Pdf:
 
     def __init__(self, pdf_path):
         self.doc = fitz.open(pdf_path)
+        self.cover = tempfile.NamedTemporaryFile()
 
-    def set_cropbox(self, out_pdf_path, rect):
+    def set_cropbox(self, rect):
         x0, y0, x1, y1 = rect
 
         page = self.doc.loadPage(0)
         page.set_cropbox(fitz.Rect(x0, y0, x1, y1))
 
-        self.doc.save(out_pdf_path)
+        self.doc.save(self.cover.name)
