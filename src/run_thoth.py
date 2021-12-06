16de104fc2f2c9503b52b30c14b9c434d1997f13
Luca Baffa <luca@openbookpublishers.com>
2021-12-06 16:53:49 +0000

    add map

--- src/run_thoth.py
+++ src/run_thoth.py
@@ -6,6 +6,9 @@ from thothlibrary import ThothClient
 # from pdf import Pdf
 # from img import Img
 
+MAP = {('156.0', '234.0'): [748, 9, 1190, 672], # royal octavo
+       ('210.0', '297.0'): [892, 9, 1487, 851], # A4
+       ('178.0', '245.0'): [264, 3, 441, 257]} # 7x10
 
 def main():
     pdf_path = path.abspath('cover.pdf')
