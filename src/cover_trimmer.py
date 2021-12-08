e4c6f89e1081cb44a8397a675f4b456f8e71fa56
Luca Baffa <luca@openbookpublishers.com>
2021-12-08 08:09:32 +0000

    compose output filename dinamically

--- src/cover_trimmer.py
+++ src/cover_trimmer.py
@@ -8,15 +8,16 @@ class Trimmer:
     Class for trimming PDF cover artwork to output images (JPEG files) of the
     front cover.
 
-    This class gets a PDF file as input and offers methods to crop the
-    front cover. Lastly, this class offers method to convert pdf covers 
-    to jpeg format with specified width.
+    This class gets a PDF file as input and offers methods to set the cropbox
+    to the size of the front cover. Lastly, this class offers method to trim
+    the PDF to the cropbox size and export the result to jpeg format with
+    specified width.
     '''
 
     def __init__(self, pdf_path, output_folder=''):
         self.doc = fitz.open(pdf_path)
         self.output_folder = output_folder
-        
+
         self.cover = tempfile.NamedTemporaryFile()
 
     def set_cropbox(self, rect):
@@ -27,11 +28,13 @@ class Trimmer:
 
         self.doc.save(self.cover.name)
 
-    def convert(self, width):
+    def convert(self, width, prefix='cover-', postfix='-RGB'):
+        file_name = f'{prefix}{width}px{postfix}'
+
         convert_from_path(self.cover.name,
                           single_file=True,
                           output_folder=self.output_folder,
                           use_cropbox=True,
                           size=(None, width),
                           fmt='jpg',
-                          output_file=f'cover-{width}px')
+                          output_file=file_name)
